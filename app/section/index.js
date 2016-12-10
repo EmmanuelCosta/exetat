2c86b280ed7819e764bb1d05a8a28efd27b7f451
ebabalac <ebabalac@gmail.com>
2016-12-10 23:01:26 +0100

    add log to section controller

--- app/section/index.js
+++ app/section/index.js
@@ -4,12 +4,14 @@ var router = new express.Router();
 var bole = require('bole');
 var log = bole('section');
 var assert = require('assert');
+var common=require('../common/common');
 
 // get all sections
 router.get('/sections',function(req, res){
       Section.find(function(err, sections) {
             if (err){
-              res.send(err);
+               log.debug("[GET]["+req.originalUrl+"]"+"unexepected error while searching by all sections. error is : "+err) ;
+               res.json({ message: 'unexpected error' });
             }else{
             res.json(sections);
           }
@@ -18,98 +20,81 @@ router.get('/sections',function(req, res){
 
 
 router.get('/section/:section_id',function(req, res) {
-      log.info(req.params.section_id +' Section has been created');
+     
         Section.findById(req.params.section_id, function(err, section) {
             if (err){
-                res.send(err);
+                 log.debug("[GET]["+req.originalUrl+"]"+"unexepected error while searching by id. error is : "+err) ;
+               res.json({ message: 'unexpected error' });
             }else{
                res.json(section);
           }
         });
 });
 
-// get the section by id id (accessed at POST http://localhost:3000/api/section/:id)
+// get the section by id id (accessed at POST http://localhost:3000/api/section/)
 router.post('/section',function(req, res) {
         
-        var jsonSection = req.body.section;
-        jsonSection= JSON.parse(jsonSection);
-        var section = new Section();
-        section.name = jsonSection.name;        
-        section.save(function(err) {         
-            if (err){
-                res.send(err);
-            }else{
-            res.json({ message: 'Section created!' });
-          }
-        });
+          if(common.isEmpty(req.body.section)){
+            res.status(400) 
+            log.debug("[POST]["+req.originalUrl+"]"+"the key section to parse is missing. The given is req.body.section ="+req.body.section) ;
+             res.json({ message: 'error with the urlencoded' });
+          }else if(common.isEmpty(req.body.section.name)){
+          var jsonSection = req.body.section;
+          jsonSection= JSON.parse(jsonSection);
+          var section = new Section();
+          section.name = jsonSection.name;        
+          section.save(function(err) {         
+              if (err){
+                   log.debug("[POST]["+req.originalUrl+"]"+"unexepected error while saving. error is : "+err) ;
+                 res.json({ message: 'unexpected error' });
+              }else{
+              res.json({ message: 'Section created!' });
+            }
+          });
+        }else{
+          log.debug("[POST]["+req.originalUrl+"]"+"the name of section to parse is missing. The given is req.body.section ="+req.body.section) ;
+          res.json({ message: 'error with the urlencoded' });
+        }
 });
 
 // add the question  (accessed at PUT http://localhost:3000/api/section)
-router.put('/section',function(req, res) {        
-          
+router.put('/section',function(req, res) {                 
 
-          if(isEmpty(req.body.section)){
-            res.status(400)  
-             res.json({ message: 'missing section key' });
+          if(common.isEmpty(req.body.section)){
+            res.status(400) 
+            log.debug("[PUT]["+req.originalUrl+"]"+"the key section to parse is missing. The given is req.body.section ="+req.body.section) ;
+             res.json({ message: 'error with the urlencoded' });
           }
             var jsonSection = req.body.section;
              jsonSection= JSON.parse(jsonSection);
-          if(isEmpty(jsonSection._id)){
-                res.status(400)  
-             res.json({ message: 'missing id of the item' });
-          }else{
+          if(common.isEmpty(jsonSection._id)||common.isEmpty(jsonSection.name)){
+              res.status(400)  
+            log.debug("[PUT]["+req.originalUrl+"]"+"the _id of section or the name to parse is missing. The given is req.body.section ="+req.body.section) ;
+             res.json({ message: 'error with the urlencoded' });
+          }else{     
 
-        
-         
            retreiveSection = Section.findById(jsonSection._id, function(err, section) {
             if (err){
-                res.status(400)
-                res.send(err);
-            }
- 
+             
+                 log.debug("[PUT]["+req.originalUrl+"]"+"unexepected error while searching by id. error is : "+err) ;
+               res.json({ message: 'unexpected error' });
+            }else{ 
                section.name=jsonSection.name;
                
                 section.save(function(err) {         
-                    if (err){
-                        res.send(err);
+                    if (err){                 
+                    log.debug("[PUT]["+req.originalUrl+"]"+"unexepected error while saving by id. error is : "+err) ;
+                   res.json({ message: 'unexpected error' });
                     }else{
-                      log.info("=======> "+section.subject[0]._id);
+                      
                     res.json({ message: 'Section updated!'});
                   }
                 });
+            }
          });
         }
-          
-
-      
      
 });
 
-// Speed up calls to hasOwnProperty
-var hasOwnProperty = Object.prototype.hasOwnProperty;
 
-function isEmpty(obj) {
-
-    // null and undefined are "empty"
-    if (obj == null) return true;
-
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-
-    // If it isn't an object at this point
-    // it is empty, but it can't be anything *but* empty
-    // Is it empty?  Depends on your application.
-    if (typeof obj !== "object") return true;
-
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-
-    return true;
-}
 module.exports=router;
